6677628b30051bb042d40a8d71b3fdce53d5a3d4
Nico Hernandez <marcnicohernandez@gmail.com>
2023-02-10 21:50:53 -0800

    logic to detect/display sensors on a particular device

--- app/src/main/java/com/example/androiddevicesensors/MainActivity.java
+++ app/src/main/java/com/example/androiddevicesensors/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.androiddevicesensors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager mySensorManager;
+
+    TextView sensorsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> mList = mySensorManager.getSensorList(Sensor.TYPE_ALL);
+        sensorsTextView = findViewById(R.id.sensorsTextView);
+        for (int i = 1; i < mList.size(); i++) {
+            sensorsTextView.append("\n" + mList.get(i).getName());
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onPause() {
+        mySensorManager.unregisterListener(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }

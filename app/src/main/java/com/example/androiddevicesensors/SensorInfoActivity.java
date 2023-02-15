a8187283203f58c09be1526374b8cf1210c211f5
Nico Hernandez <marcnicohernandez@gmail.com>
2023-02-14 19:40:25 -0800

    activity where a specific sensor's values are displayed

---
+++ app/src/main/java/com/example/androiddevicesensors/SensorInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.androiddevicesensors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SensorInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_info);
+    }
+}

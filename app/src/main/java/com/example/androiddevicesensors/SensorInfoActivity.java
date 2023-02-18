a7a2d51e236169be6c2b0cfff61b56ca09ed264f
Nico Hernandez <47044633+nicohsfu@users.noreply.github.com>
2023-02-17 16:34:09 -0800

    display sensor values via index position passed down from sent intent

--- app/src/main/java/com/example/androiddevicesensors/SensorInfoActivity.java
+++ app/src/main/java/com/example/androiddevicesensors/SensorInfoActivity.java
@@ -2,18 +2,61 @@ package com.example.androiddevicesensors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class SensorInfoActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SensorInfoActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager mySensorManager;
+
+    Sensor sensor;
+
+    TextView sensorValsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_info);
 
+        sensorValsTextView = findViewById(R.id.sensorValsTextView);
+
         int i = getIntent().getIntExtra("POSITION", -1);
 
-        Toast.makeText(this, "" + i, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "index position: " + i, Toast.LENGTH_SHORT).show();
+
+        mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> mList = mySensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        sensor = mList.get(i);
     }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mySensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] vals = event.values;
+        String s = "";
+        for(int i = 0; i < vals.length; i++){
+            s += ("" + vals[i] + "\n");
+        }
+        sensorValsTextView.setText(s);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 }

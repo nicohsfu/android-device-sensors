8ad79862bb94bc5457645786c38ddda5804be916
Nico Hernandez <marcnicohernandez@gmail.com>
2023-02-26 13:59:17 -0800

    implement click listener on movement Activity

--- app/src/main/java/com/example/androiddevicesensors/MovementActivity.java
+++ app/src/main/java/com/example/androiddevicesensors/MovementActivity.java
@@ -3,12 +3,25 @@ package com.example.androiddevicesensors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MovementActivity extends AppCompatActivity {
+public class MovementActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button movementStatusButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movement);
+
+        movementStatusButton = findViewById(R.id.movementStatusButton);
+        movementStatusButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "moving? button clicked", Toast.LENGTH_SHORT).show();
     }
 }

c219054d5a3c27de7fbf06d31820ead396329349
Nico Hernandez <marcnicohernandez@gmail.com>
2023-02-26 13:05:24 -0800

    pass sensor name as intent to the specific sensor Activity

--- app/src/main/java/com/example/androiddevicesensors/CustomAdapter.java
+++ app/src/main/java/com/example/androiddevicesensors/CustomAdapter.java
@@ -69,15 +69,16 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(context,
-                    "You have clicked " + ((TextView) view.findViewById(R.id.sensorNameTextView)).getText().toString(),
-                    Toast.LENGTH_SHORT).show();
+//            Toast.makeText(context,
+//                    "You have clicked " + ((TextView) view.findViewById(R.id.sensorNameTextView)).getText().toString(),
+//                    Toast.LENGTH_SHORT).show();
 
             // next steps:
             // create intent - send to new Values activity
 //             send value's / sensor's index - putExtras - position variable
             Intent i = new Intent(context, SensorInfoActivity.class); // this is erroring out for now
             i.putExtra("POSITION", position);
+            i.putExtra("SENSORNAME", sensorNameTextView.getText().toString());
             view.getContext().startActivity(i); // this is erroring out for now
         }
     }
